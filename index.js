db9795ff746c7da3e827ba3db9621c50583a8097
gilangif <gilangidulfitri1201@gmail.com>
2026-02-23 13:00:36 +0700

    init

--- index.js
+++ index.js
@@ -110,7 +110,7 @@ if (cluster.isPrimary) {
   const app = express()
   const port = process.env.PORT || 3003
 
-  app.use(cors())
+  // app.use(cors())
   app.use(express.json({ limit: "200mb" }))
   app.use(express.urlencoded({ limit: "200mb", extended: true }))
 
