8f1dcf06da82a9ead3bb754e6cca6b6e39b0b6c6
gilangif <gilangidulfitri1201@gmail.com>
2026-02-23 13:10:35 +0700

    init

--- db/supabase.js
+++ db/supabase.js
@@ -1,6 +1,6 @@
 import { Pool } from "pg"
 
-const db = new Pool({ user: "cyborg", host: "159.89.202.146", database: "cyborg", password: "cyborg", port: 5432 })
-// const db = new Pool({ user: "cyborg", host: "localhost", database: "cyborg", password: "cyborg", port: 5432 })
+// const db = new Pool({ user: "cyborg", host: "159.89.202.146", database: "cyborg", password: "cyborg", port: 5432 })
+const db = new Pool({ user: "cyborg", host: "localhost", database: "cyborg", password: "cyborg", port: 5432 })
 
 export default db
--- models/Group.js
+++ models/Group.js
@@ -61,9 +61,6 @@ class Group {
 
     params.push(limit, offset)
 
-    console.log(qd)
-    console.log(params)
-
     const qr = `SELECT COUNT(*) FROM groups ${where};`
 
     const { rows, rowCount: total_data } = await db.query(qd, params)
